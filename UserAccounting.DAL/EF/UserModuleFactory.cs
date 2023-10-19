7c7b231c85f7548866e127b1d318181f3d4b8c05
mo0nchild <happytown77831@gmail.com>
2023-10-19 09:05:46 +0300

    feat: initialize database migration

---
+++ UserAccounting.DAL/EF/UserModuleFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserAccounting.DAL.EF
+{
+    internal sealed class UserModuleFactory : IDesignTimeDbContextFactory<UserModuleContext>
+    {
+        public UserModuleFactory() : base() { }
+        public UserModuleContext CreateDbContext(string[] args)
+        {
+            var optionBuilder = new DbContextOptionsBuilder<UserModuleContext>();
+            optionBuilder.UseNpgsql("Host=localhost;Database=vgtucontest;Username=postgres;Password=prolodgy778");
+
+            return new UserModuleContext(optionBuilder.Options);
+        }
+    }
+}
---
+++ UserAccounting.DAL/Migrations/20231019060354_Init.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace UserAccounting.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "roles",
+                columns: table => new
+                {
+                    roleid = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    type = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("roles_pkey", x => x.roleid);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user_login_data",
+                columns: table => new
+                {
+                    userid = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    loginname = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    passwordhash = table.Column<string>(type: "character varying(250)", maxLength: 250, nullable: false),
+                    role = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("user_login_data_pkey", x => x.userid);
+                    table.ForeignKey(
+                        name: "user_login_data_role_fkey",
+                        column: x => x.role,
+                        principalTable: "roles",
+                        principalColumn: "roleid");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_login_data_role",
+                table: "user_login_data",
+                column: "role");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "user_login_data");
+
+            migrationBuilder.DropTable(
+                name: "roles");
+        }
+    }
+}
